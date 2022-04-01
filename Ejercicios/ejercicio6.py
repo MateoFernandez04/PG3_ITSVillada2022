8c589c22b199b09a037f7f6c587497158081815d
Mateo <emanuel040812@gmail.com>
2022-04-01 08:07:56 -0300

    declarando variables

--- Ejercicios/ejercicio6.py
+++ Ejercicios/ejercicio6.py
@@ -1,11 +1,11 @@
-def contar_vocales(cadena):
-	contador = 0
+def contar_vocales(cadena: str):
+	contador: int = 0
 	for letra in cadena:
 		if letra.lower() in "aeiou":
 			contador += 1
 	return contador
 
 print("Coloque un texto")
-cadena = input()
-cantidad = contar_vocales(cadena)
-print(f"En la cadena '{cadena}'' hay {cantidad} vocales")
+cadena: str = input()
+cantidad: int = contar_vocales(cadena)
+print(f"En la cadena '{cadena}' hay {cantidad} vocales")
