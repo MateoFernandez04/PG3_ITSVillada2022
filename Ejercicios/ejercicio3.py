745488cc940fa775e9abe0e8b5963f5289c65e1b
Mateo <emanuel040812@gmail.com>
2022-04-01 08:58:00 -0300

    arreglo de print

--- Ejercicios/ejercicio3.py
+++ Ejercicios/ejercicio3.py
@@ -1,6 +1,6 @@
 anchura = int(input("Anchura del rectángulo: "))
 altura = int(input("Altura del rectángulo: "))
-caracter =input("Altura del rectángulo: ")
+caracter =input("Caracter a escribir: ")
 for i in range(altura):
     for j in range(anchura):
         print(caracter," ",end="")
