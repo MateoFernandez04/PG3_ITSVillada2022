10f7899b09db24042eaf0ebc2d1e4abcb4562555
Mateo <emanuel040812@gmail.com>
2022-04-08 08:54:14 -0300

    Adhesion de rombo

--- Ejercicios/ejercicio_extra.py
+++ Ejercicios/ejercicio_extra.py
@@ -10,6 +10,10 @@ if option ==1:
     for i in range(num):
         print(" " *(num -i -1)+ caracter *(2 *i +1))
 else:
-    for i in range(num):
-        print(" " *(num +(i+num) -1)+ caracter *(2 *(i+aux) -1))
-        aux=aux-2
+    if option==2:
+        for i in range(num):
+            print(" " *(num +(i+num) -1)+ caracter *(2 *(i+aux) -1))
+            aux=aux-2
+    else:
+        result1=[" "*(num-i)+"*"*(i+i-1) for i in range(1,num+1)]
+        print("\n".join(result1+list(reversed(result1[:-1]))))
