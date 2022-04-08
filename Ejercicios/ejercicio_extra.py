62103d3e9b09dab40a06f7bac34d16a529645952
Mateo <emanuel040812@gmail.com>
2022-04-08 11:11:21 -0300

    Agregado rombo

--- Ejercicios/ejercicio_extra.py
+++ Ejercicios/ejercicio_extra.py
@@ -4,6 +4,8 @@ caracter:str =input("Caracter a escribir: ")
 print("1-triangulo Normal")
 
 print("2-triangulo invertido")
+
+print("3-Rombo")
 option:int=int(input("Coloque opción a hacer: "))
 aux:int=num
 if option ==1:
@@ -15,5 +17,5 @@ else:
             print(" " *(num +(i+num) -1)+ caracter *(2 *(i+aux) -1))
             aux=aux-2
     else:
-        result1=[" "*(num-i)+"*"*(i+i-1) for i in range(1,num+1)]
+        result1=[" "*(num-i)+caracter *(i+i-1) for i in range(1,num+1)]
         print("\n".join(result1+list(reversed(result1[:-1]))))
