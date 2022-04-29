68712bcf3a52311bfb9bd9950101191ec66f967f
Mateo <emanuel040812@gmail.com>
2022-04-29 10:14:47 -0300

    ejercicio1 arreglado

--- Ejercicios/ejercicio1.py
+++ Ejercicios/ejercicio1.py
@@ -4,14 +4,19 @@ listavalores: List[int]=[10, 56, 23, 120, 94]
 print(listavalores)
 print("Escriba el valor a  buscar")
 numero: int =int(input())
-def buscar(lista: List[int] , num: int):
-    print(lista)
-    print(num)
-    if num in lista: 
-         indice=(lista.index(num))
-    else:
-        indice=("No hay")
-    return indice 
+def BinarySearch(lys, val):
+    first = 0
+    last = len(lys)-1
+    index = -1
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lys[mid] == val:
+            index = mid
+        else:
+            if val<lys[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
 
-
-print("Índice:", buscar(listavalores,numero))
+print("Índice:", BinarySearch(listavalores,numero))
