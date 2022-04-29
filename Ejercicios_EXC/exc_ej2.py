e5264556c955b491a51577679a162422a1dae1d9
Mateo <emanuel040812@gmail.com>
2022-04-29 12:04:11 -0300

    Agregado ejercicio2 exc

---
+++ Ejercicios_EXC/exc_ej2.py
@@ -0,0 +1,12 @@
+def division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print('¡No se puede dividir por cero!')
+    else:
+        print(f"La division entre'{a}' y {b} da {result} ")
+
+num = int(input("Coloque un numero: "))
+num2 = int(input("Coloque otro numero"))
+
+division(num,num2)
