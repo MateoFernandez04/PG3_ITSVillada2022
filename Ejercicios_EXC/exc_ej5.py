4c35885a10d48bafb7f175be6204f4fc1a9de105
Mateo <emanuel040812@gmail.com>
2022-05-05 20:05:27 -0300

    Arreglo ejercicio 5

--- Ejercicios_EXC/exc_ej5.py
+++ Ejercicios_EXC/exc_ej5.py
@@ -1,5 +1,18 @@
-f = open ('holamundo.txt','w')
-try:
-    f.write(1)
-except TypeError:
-    print("No se puede escribir en el archivo correspondiente")
+while True:
+    try:
+        ingreso = input("Ingresar string: ")
+        datos = open('./Ejercicios_EXC/texto.txt', 'a')
+
+        if ingreso.isnumeric():
+            datos.write(int(ingreso))
+        else:
+            datos.write(ingreso)
+        while True:
+            print("Quiere seguir escribiendo: /n")
+            ans=input("ingrese opción Yes=s // No=n: ")
+            if ans == "s" or ans == "n":
+                break;
+        if ans=="n":
+            break  
+    except TypeError:
+        print("No intente ingresar numeros")
