d3263272cf2293dbc1f5b653fe1cae2c1d331cb9
Mateo <emanuel040812@gmail.com>
2022-05-13 08:33:53 -0300

    Arreglo ejercicio 4

--- Ejercicios_EXC/exc_ej4.py
+++ Ejercicios_EXC/exc_ej4.py
@@ -1,10 +1,15 @@
-try:
-    num1=int(input("Ingrese primer numero:"))
-    num2=int(input("Ingrese segundo numero:"))
-except ZeroDivisionError:
-    print("No se puede dividir por cero.")
-except ValueError:
-    print("Los valores a ingresar deben ser enteros")
-else:
-    division=num1/num2
-    print("El resultado de la división es", division)
+while True:
+    try:
+        num1=int(input("Ingrese primer numero:"))
+        num2=int(input("Ingrese segundo numero:"))
+    except ZeroDivisionError:
+        print("No se puede dividir por cero.")
+    except ValueError:
+        print("Los valores a ingresar deben ser enteros.")
+    else:
+        division=num1/num2
+        print("El resultado de la división es", division)
+
+        option = input("¿Desea continuar? (s/n) ")
+        if option == "n":
+            break
