26889941bc3b5bfbd3ece3ec1272682f036b4543
Mateo <emanuel040812@gmail.com>
2022-05-13 08:41:28 -0300

    Arreglo ejercicio 3

--- Ejercicios_EXC/exc_ej3.py
+++ Ejercicios_EXC/exc_ej3.py
@@ -1,7 +1,11 @@
-meses=("enero","febrero","marzo","abril","mayo","junio",
-       "julio","agosto","septiembre","octubre","noviembre","diciembre")
-try:
-    indice=int(input("Ingrese un número de mes:"))
-    print(meses[indice-1])
-except IndexError:
-    print("No es un índice de un mes")
+meses=("enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre")
+while True:
+    try:
+        indice=int(input("Ingrese un número de mes:"))
+        print(meses[indice-1])
+    except IndexError:
+        print("No es un índice de un mes")
+
+    flag = input("¿Desea continuar? (S/N)")
+    if flag == "N":
+        break
